2bd8fd63ed68fbf54bed3b6ad3a8321a9aadfcd8
ShrekCoding21 <sreekar.1105@gmail.com>
2024-11-15 22:15:00 -0500

    added non-functional platform class
    already built player class moved to own Player folder containing Json files, sprites, etc.
    
    non-functional platform class created (too lazy to test it fully until it works)

---
+++ Player/player.py
@@ -0,0 +1,97 @@
+import pygame
+
+class Player():
+
+
+    def __init__(self, player_name, position, controls, color):
+        super().__init__()
+        self.player_name = player_name
+        self.position = pygame.Vector2(position)
+        self.velocity = pygame.Vector2(0, 0)
+        self.on_ground = False
+        # self.is_sliding = False
+        self.controls = {action: getattr(pygame, key) for action, key in controls.items()}
+        # self.animations = self.load_animations()
+        # self.current_animation = self.animations["idle"]
+        # self.running_speed = 1
+        # self.shield = False
+        # self.double_jump = False
+        # self.effects = []
+        # self.powerups = []
+
+        "temp variables until we can add player sprites"
+
+        self.width = 32
+        self.height = 64
+        self.color = color
+
+    def names(self):
+
+        player_names = []
+        player_ids = []
+        for id_num in range(2): 
+            player_num = id_num + 1
+            invalid = True
+
+            while invalid:
+                player_name = input(f"Player {player_num}, please enter your name")
+
+                if player_name != '' and len(player_name) < 21:
+                    if player_name not in player_names:
+                        player_ids.append(f'player{player_num}')
+                        player_names.append(player_name)
+                        invalid = False
+                    else:
+                        print("That name is already being used, please choose a different one.")
+                else:
+                    print("Invalid name. Please choose a name smaller than 21 characters and not blank.")
+      
+        return player_names, player_ids
+    
+    def collisions(self):
+        pass
+    
+    def load_animations(self):
+        pass
+
+    def update_animations(self):
+        pass
+
+    def jump(self):
+        
+        if self.on_ground:
+            JUMP_STRENGTH = -10
+            self.velocity.y = JUMP_STRENGTH
+            self.on_ground = False
+
+    def handle_controls(self, keys):
+        
+        if keys[self.controls['left']]:
+            self.velocity.x = -25
+        elif keys[self.controls['right']]:
+            self.velocity.x = 25
+        else:
+            self.velocity.x = 0
+
+        if keys[self.controls['jump']] and self.on_ground:
+            self.jump()
+
+    def update(self, delta_time, keys):
+
+        self.gravity_and_motion(delta_time)
+        self.handle_controls(keys)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    @property
+    def rect(self):
+        return pygame.Rect(self.position.x, self.position.y, self.width, self.height)
+
+    def gravity_and_motion(self, delta_time):
+
+        GRAVITY = 0.5
+        if not self.on_ground:
+            self.velocity.y += GRAVITY
+        self.position.x += self.velocity.x * delta_time
+        self.position.y += self.velocity.y * delta_time
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import asyncio
 import json
 import datetime
-import os
+from Player.player import Player
 
 WEB_ENVIRONMENT = False
 try:
@@ -29,131 +29,92 @@ async def load_json_file(filepath):
             keys_data = json.load(key_map)
     return keys_data
 
-class Player():
+class Platform():
+    
 
+    def __init__(self, position, is_moving, movement_range, speed, direction, image, width, height):
 
-    def __init__(self, player_name, position, controls, color):
-        super().__init__()
-        self.player_name = player_name
+        self.start_position = pygame.Vector2(position)
         self.position = pygame.Vector2(position)
-        self.velocity = pygame.Vector2(0, 0)
-        self.on_ground = False
-        # self.is_sliding = False
-        self.controls = {action: getattr(pygame, key) for action, key in controls.items()}
-        # self.animations = self.load_animations()
-        # self.current_animation = self.animations["idle"]
-        # self.running_speed = 1
-        # self.shield = False
-        # self.double_jump = False
-        # self.effects = []
-        # self.powerups = []
+        self.is_moving = is_moving
+        self.movement_range = pygame.Vector2(movement_range)
+        self.speed = speed
+        self.animation_frames = []
+        self.current_frame = 0
+        self.frame_count = len(self.animation_frames)
+        self.direction = direction
+        self.image = image
+        self.color = (255, 255, 255)
+        self.width, self.height = width, height
 
-        "temp variables until we can add player sprites"
-
-        self.width = 32
-        self.height = 64
-        self.color = color
-
-    def names(self):
-
-        player_names = []
-        player_ids = []
-        for id_num in range(2): 
-            player_num = id_num + 1
-            invalid = True
-
-            while invalid:
-                player_name = input(f"Player {player_num}, please enter your name")
-
-                if player_name != '' and len(player_name) < 21:
-                    if player_name not in player_names:
-                        player_ids.append(f'player{player_num}')
-                        player_names.append(player_name)
-                        invalid = False
-                    else:
-                        print("That name is already being used, please choose a different one.")
-                else:
-                    print("Invalid name. Please choose a name smaller than 21 characters and not blank.")
-      
-        return player_names, player_ids
-    
-    def collisions(self):
-        pass
-    
-    def load_animations(self):
-        pass
-
-    def update_animations(self):
-        pass
-
-    def jump(self):
-        
-        if self.on_ground:
-            JUMP_STRENGTH = -10
-            self.velocity.y = JUMP_STRENGTH
-            self.on_ground = False
-
-    def handle_controls(self, keys):
-        
-        if keys[self.controls['left']]:
-            self.velocity.x = -25
-        elif keys[self.controls['right']]:
-            self.velocity.x = 25
-        else:
-            self.velocity.x = 0
-
-        if keys[self.controls['jump']] and self.on_ground:
-            self.jump()
-
-    def update(self, delta_time, keys):
-
-        self.gravity_and_motion(delta_time)
-        self.handle_controls(keys)
+    def update(self, dt):
+        if self.is_moving:
+            self.posiiton += self.direction * self.speed * dt
+            if self.position.distance_to(self.start_position) > self.movement_range.length():
+                self.direction *= -1
+            self.current_frame = (self.current_frame + 1) % self.frame_count
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        if self.animation_frames:
+            frame = self.animation_frames[self.current_frame]
+            screen.blit(frame, self.position)
+        else:
+            pygame.draw.rect(screen, self.image, (*self.position, self.width, self.height))
+    
+    def set_image(self, image_path):
 
-    @property
-    def rect(self):
-        return pygame.Rect(self.position.x, self.position.y, self.width, self.height)
+        if image_path != None:
+            image = pygame.image.load(image_path)
+            self.animation_frames = [image]
+            self.frame_count = len(self.animation_frames)
+        else:
+            image = self.color
+        
+        return image
+    
+    def set_animation_frames(self, image_paths):
+        self.animation_frames = [pygame.image.load(path) for path in image_paths]
+        frame_count = len(self.animation_frames)
 
-    def gravity_and_motion(self, delta_time):
+    def reset(self):
+        self.position = pygame.Vector2(self.start_position)
+        self.current_frame = 0
+    
 
-        GRAVITY = 0.5
-        if not self.on_ground:
-            self.velocity.y += GRAVITY
-        self.position.x += self.velocity.x * delta_time
-        self.position.y += self.velocity.y * delta_time
 
-class Platform():
-    pass
+    
 
 class Powerups():
     pass
 
 async def main():
 
-    keys_data = await load_json_file('player_controls.json')
+    keys_data = await load_json_file('player/player_controls.json')
 
     player1_controls = keys_data['controls']['players']['player1']
     player2_controls = keys_data['controls']['players']['player2']
 
+    platform = Platform(position=(100, 400), is_moving = False, width=200, height=50, speed=100, direction=pygame.Vector2(1, 0), image=None, movement_range=pygame.Vector2(300, 0))
     player1 = Player(player_name="Player 1", position=(100, 100), controls=player1_controls, color=(0, 255, 0))
     player2 = Player(player_name="Player 2", position=(200, 100), controls=player2_controls, color=(0, 0, 255))
 
     running = True
     while running:
-        delta_time = clock.tick(60) / 1000.0
+        dt = clock.tick(60) / 1000.0
         keys = pygame.key.get_pressed()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        player1.update(delta_time, keys)
-        player2.update(delta_time, keys)
+        dt = clock.get_time() / 1000.0
+        platform.update(dt)
+
+        player1.update(dt, keys)
+        player2.update(dt, keys)
 
         screen.fill((0, 0, 0))
+        platform.draw(screen)
         player1.draw(screen)
         player2.draw(screen)
         pygame.display.flip()
